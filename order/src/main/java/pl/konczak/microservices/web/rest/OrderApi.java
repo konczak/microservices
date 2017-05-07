7e522a4018ccd07e9463f70e7b05a538b609b77d
Piotr Konczak <konczak.piotrek@gmail.com>
2017-05-07 13:57:10 +0200

    add missing logs in OrderApi

--- order/src/main/java/pl/konczak/microservices/web/rest/OrderApi.java
+++ order/src/main/java/pl/konczak/microservices/web/rest/OrderApi.java
@@ -2,6 +2,8 @@ package pl.konczak.microservices.web.rest;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,8 @@ import pl.konczak.microservices.domain.Order;
 @RequestMapping("/order")
 public class OrderApi {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderApi.class);
+
     private final IOrderRepository orderRepository;
 
     private final IOrderBO orderBO;
@@ -29,12 +33,14 @@ public class OrderApi {
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Order> list() {
+        LOGGER.info("GET /order");
         return orderRepository.findAll();
     }
 
     @RequestMapping(method = RequestMethod.GET,
                     value = "/{id}")
     public HttpEntity<Order> get(@PathVariable("id") Long id) {
+        LOGGER.info("GET /order/{}", id);
         Order order = orderRepository.getOne(id);
 
         if (order == null) {
@@ -46,6 +52,7 @@ public class OrderApi {
 
     @RequestMapping(method = RequestMethod.POST)
     public HttpEntity<Order> order(@RequestBody OrderCreateCommand orderCreateCommand) {
+        LOGGER.info("POST /order");
         Order order = orderBO.create(orderCreateCommand.getCustomerId(), orderCreateCommand.getProductIds());
         return ResponseEntity.ok(order);
     }
